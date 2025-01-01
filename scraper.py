5eb80e42691f3c079fb90c559596d4a5fd11f2ef
Joe Thorne <jgthorne86@gmail.com>
2025-01-01 10:32:47 +1000

    Scraper refactored, no works on both Program and FOS pages

--- scraper.py
+++ scraper.py
@@ -8,31 +8,53 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.wait import WebDriverWait
 from course import Course 
 
-def start_driver():
+PROG_PAGE_URL = "https://programs-courses.uq.edu.au/requirements/program/2350/2023"
+SOFT_ENG_PAGE_URL = "https://programs-courses.uq.edu.au/requirements/plan/SOFTEX2350/2023"
+
+PROG_PAGE_MAP = {"Core": "part-A", 
+                "Prep": "part-C",
+                "FYElec": "part-D"} 
+
+SOFT_ENG_PAGE_MAP = {"FOS Compulsory": "part-A", 
+                     "FOS Elective": "part-B",
+                     "FOS Masters": "part-C",
+                     "FOS Breadth": "part-D",
+                     "FOS Breadth": "part-D"} 
+
+def start_driver(options, url):
+
+    driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
+    driver.implicitly_wait(10)
+    driver.get(url)
+
+    return driver
+
+def parse_element(section, category, courses):
+    subjects = section.find_elements(By.CLASS_NAME, "curriculum-reference")
+    
+    for subject in subjects:
+        courses.append(Course(subject.get_attribute("href").split('=')[1],
+                              subject.get_attribute("title"), 2, category))
+
+def scrape_page(url, pageMap, courses):
+    driver = start_driver(options, url)    
+
+    for key in pageMap:
+        section = driver.find_element(By.ID, pageMap[key])
+        parse_element(section, pageMap[key], courses)
+    
+    driver.close()
+
+if __name__ == "__main__":
     options = Options()
     options.add_argument('--headless')
     options.add_argument('--disable-dev-shm-usage')
     options.add_argument('--no-sandbox')
 
-    driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
-    driver.implicitly_wait(10)
-    driver.get("https://programs-courses.uq.edu.au/requirements/program/2350/2023")
-
-    return driver
-
-if __name__ == "__main__":
-    driver = start_driver()
-
-    section = driver.find_element(By.ID, "part-A")
-    subjects= section.find_elements(By.CLASS_NAME, "curriculum-reference")
-    
     courses = []
-    for subject in subjects:
-        courses.append(Course(subject.get_attribute("href").split('=')[1], subject.get_attribute("title"), 2, "Core"))
+    scrape_page(PROG_PAGE_URL, PROG_PAGE_MAP, courses)
+    scrape_page(SOFT_ENG_PAGE_URL, SOFT_ENG_PAGE_MAP, courses)
 
     for course in courses:
         print(course)
         print(" ")
-
-        
-    driver.close()
