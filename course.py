9842e7fc1af5160f5e114314941ca75075f55683
Joe Thorne <jgthorne86@gmail.com>
2025-01-07 21:42:43 +1000

    Added data to Course.py, refactored scraper to allow operation as imported module or when run directly

--- course.py
+++ course.py
@@ -7,18 +7,39 @@ class Course:
         self._name = name
         self._units = int(units)
         self._category = category
+        self._level = None
+        self._faculty = None
+        self._school = None
+        self._prereqs = None
 
     def __str__(self):
         return f"{self._code}: {self._name} \n{self._units} units \n{self._category}"
 
+    def print_full(self):
+        print(f"{self._code}: {self._name} \n{self._units} units \n{self._category}")
+        print(self._level)
+        print(self._faculty)
+        print(self._school)
+        print("Prerequisites: " + str(self._prereqs))
+
     def get_code(self):
         return self._code
-
     def get_name(self):
         return self._name
-
     def get_units(self):
         return self._units
-    
     def get_category(self):
         return self._category
+    def get_level(self):
+        return self._level
+    def get_faculty(self):
+        return self._faculty
+    def get_school(self):
+        return self._school
+    def get_prereqs(self):
+        return self._prereqs
+    def set_details(self, level, faculty, school, prereqs):
+        self._level = level
+        self._faculty = faculty
+        self._school = school
+        self._prereqs = prereqs
--- scraper.py
+++ scraper.py
@@ -5,7 +5,6 @@ from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.by import By
-from selenium.webdriver.support.wait import WebDriverWait
 from course import Course 
 
 PROG_PAGE_URL = "https://programs-courses.uq.edu.au/requirements/program/2350/2023"
@@ -22,8 +21,11 @@ SOFT_ENG_PAGE_MAP = {"FOS Compulsory": "part-A",
                      "FOS Breadth": "part-D"} 
 COURSE_BASE_URL = "https://programs-courses.uq.edu.au/course.html?course_code="
 
-def start_driver(options):
-
+def start_driver():
+    options = Options()
+    options.add_argument('--headless')
+    options.add_argument('--disable-dev-shm-usage')
+    options.add_argument('--no-sandbox')
     driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
     driver.implicitly_wait(2)
 
@@ -46,36 +48,30 @@ def scrape_course_list(driver, url, pageMap, courses):
         parse_section(section, key, courses)
     
 
-def scrape_course_details(driver, courses, options):
+def scrape_course_details(driver, courses):
     for course in courses:
         driver.get(COURSE_BASE_URL + course.get_code())
         level = driver.find_element(By.ID, "course-level").text
         faculty = driver.find_element(By.ID, "course-faculty").text
         school = driver.find_element(By.ID, "course-school").text
         prereqList = driver.find_elements(By.ID, "course-prerequisite")
-        prereq = None
-        if prereqList != []:
-            prereq = prereqList[0].text
-
-        print(course)
-        print(level)
-        print(faculty)
-        print(school)
-        print("Prerequisites: " + str(prereq))
-        print("===============================================================")
-
-if __name__ == "__main__":
-    options = Options()
-    options.add_argument('--headless')
-    options.add_argument('--disable-dev-shm-usage')
-    options.add_argument('--no-sandbox')
-    driver = start_driver(options)
+        prereq = None if prereqList == [] else prereqList[0].text
+        course.set_details(level, faculty, school, prereq)
+        
+def get_courses():
+    driver = start_driver()
     
     courses = []
     
     scrape_course_list(driver, PROG_PAGE_URL, PROG_PAGE_MAP, courses)
     scrape_course_list(driver, SOFT_ENG_PAGE_URL, SOFT_ENG_PAGE_MAP, courses)
-
-    scrape_course_details(driver, courses, options)
+    scrape_course_details(driver, courses)
 
     driver.close()
+    return courses
+
+if __name__ == "__main__":
+    courses = get_courses()
+    for course in courses:
+        course.print_full()
+        print("=======================================================")
